4916fad6d8a6783d2924d71e76f88104ba659275
Julius <33183774+juliusfrost@users.noreply.github.com>
2026-03-27 19:11:45 -0700

    chore: add GPL-3.0 copyright header to all Python source files

--- src/ollim_bot/agent_streaming.py
+++ src/ollim_bot/agent_streaming.py
@@ -1,3 +1,17 @@
+# ollim-bot
+# Copyright (C) 2025-2026 Julius Frost
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 """Streaming response consumer -- extracted from Agent.stream_chat."""
 
 from __future__ import annotations
